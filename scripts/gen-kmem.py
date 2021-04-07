fae34b565a21e335086df95d60c602f2ef12a713
Paul Giacopelli <paul.giacopelli@krono-safe.com>
2021-04-07 12:02:23 +0200

    added script to generate a custom memory placement

--- scripts/gen-kmem.py
+++ scripts/gen-kmem.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
 #
 # This script generates the json files for the memory placement. It allows to control the placement of two parts:
-# * the task;
-# * the co_runners;
+#  * the task;
+#  * the co_runners;
 #
 # Usage:
-#   python3 gen-corunner.py --jumps 1024 > code.asm
+#   python3 gen-kmem.py --config {json config file} --memreport {ks memreport file} --default_kmem {default kmemory json file}
 #
-# both the data and the text can be set.
+# Notes:
+#   * The default kmemory will be overwritten. To avoid that, add --out_kmem to specify an other output file.
+#   * An exemple template for the json config file is available at /exemples/mem-place.json. In each sections, at least of address or region must be present. The names of the elements must be the task names for agents and the identifiers in the default kmem for the corunners.
+#
+# Both the data and the text can be set separatly.
 
 
 import argparse
@@ -20,18 +24,21 @@ from pathlib import Path
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', nargs='?', type=argparse.FileType('r'), default='-')
 parser.add_argument('--memreport', type=Path, required=True)
-parser.add_argument('--default_kmem', type=Path, nargs='+')
+parser.add_argument('--default_kmem', type=Path, required=True)
+parser.add_argument('--out_kmem', type=Path)
 parser.add_argument("--kdbv", type=Path, required=True)
 args = parser.parse_args(sys.argv[1:])
 
-def del_all_list(l):
-  n = range(0, len(l))
-  for i in n:
-    del l[i]
+if not args.out_kmem:
+  args.out_kmem = args.default_kmem
+
+def del_all_list(l, rem):
+  for el in l:
+    rem.remove(el)
 
 memreport = load_db(args.kdbv, args.memreport)
 kmem = load_json(args.default_kmem)
-config = load_json(args.config)
+config = load_json(args.config, o=False)
 
 sec_sat = dict()
 for sec in memreport['sections']:
@@ -45,65 +52,73 @@ for sec in memreport['sections']:
 for el in config['elements']:
   assert el['type'] in ['corunner', 'task']
   secs = []
-  del_dom = []
   if el['type'] == 'corunner':
-    for reg in kmem['kmemory']['region']:
-      for dom in reg['domains']:
-        if dom['identifier'] in el['names']:
-          d = set()
-          for sec in dom['output_sections']:
-            d.add(sec['name'])
-          secs.append([d, deepcopy(dom), reg['name']])
-          del_dom.append(dom)
+    for reg in kmem['kmemory']['regions']:
+      if 'domains' in reg.keys():
+        del_dom = []
+        for dom in reg['domains']:
+          if 'identifier' in dom.keys() and dom['identifier'] in el['names']:
+            d = set()
+            for sec in dom['output_sections']:
+              d.add(sec['name'])
+            secs.append([d, deepcopy(dom), reg['name']])
+            del_dom.append(dom)
+        del_all_list(del_dom, reg['domains'])
   else:
     sec_sat2 = deepcopy(sec_sat)
     for sec in sec_sat.keys():
       if sec_sat[sec][3] not in el['names']:
         del sec_sat2[sec]
 
-    for reg in kmem['kmemory']['region']:
-      for dom in reg['domains']:
-        d = set()
-        for sec in dom['output_sections']:
-          if sec['name'] in sec_sat2.keys():
-            d.add(sec['name'])
-        if d:
-          secs.append([d, deepcopy(dom), reg['name']])
-          del_dom.append(dom)
-  del_all_list(del_dom)
+    for reg in kmem['kmemory']['regions']:
+      if 'domains' in reg.keys():
+        del_dom = []
+        for dom in reg['domains']:
+          d = set()
+          for sec in dom['output_sections']:
+            if sec['name'] in sec_sat2.keys():
+              d.add(sec['name'])
+          if d:
+            secs.append([d, deepcopy(dom), reg['name']])
+            del_dom.append(dom)
+        del_all_list(del_dom, reg['domains'])
+  if not secs:
+    continue
 
   for sec in el['sections']:
     assert set(sec.keys()).intersection({'region','address'}), \
       "A least one of 'region', 'address' must be present in placement configuration"
     d = set()
     comp = False
-    secs_2 = deep_copy(secs)
+    secs_2 = deepcopy(secs)
     del_s = []
     for s in secs_2:
       for n in s[0]:
-        if sec_sat[n][2] in sec['types']:
+        if sec_sat[n][2] in sec['names']:
           d.add(n)
-        else:
-          for os in s[1]['output_sections']:
-            if os['name'] == n:
-              del os
+#        else:
+#          for os in s[1]['output_sections']:
+#            if os['name'] == n:
+#              del os
       inter = d.intersection(s[0])
       if not inter:
         del_s.append(s)
-    del_all_list(del_s)
+    del_all_list(del_s, secs_2)
 
     def find_reg_name():
-      for reg in kmem['kmemory']['region']:
+      for reg in kmem['kmemory']['regions']:
         addr = reg['physical_address']
         if addr <= sec['address'] < addr+reg['size']:
           return reg['name']
     reg_name = sec['region'] if 'region' in sec.keys() else find_reg_name()
-    for reg in kmem['kmemory']['region']:
+    for reg in kmem['kmemory']['regions']:
       if reg['name'] == reg_name:
         if 'address' not in sec.keys():
+          if 'domains' not in reg.keys():
+            reg['domains'] = list()
+          if not reg['domains']:
+            secs_2[0][1]['output_sections'][0]['physical_address'] = reg['physical_address']
           for s in secs_2:
-            if not reg['domains']:
-              s[1]['output_sections'][0]['physical_address'] = reg['physical_address']
             reg['domains'].append(s[1])
         else:
           addr1 = reg['physical_address']
@@ -128,7 +143,7 @@ for el in config['elements']:
                 if addr1 < sec['address'] < addr2+sec_sat[os['name']][0]:
                   os0 = reg['domains'][i]['output_sections'][0]
                   os0['physical_address'] = sec_sat[os0['name']][1]
-                  secs_2[0]['output_sections'][0]['physical_address'] = sec['address']
+                  secs_2[0][1]['output_sections'][0]['physical_address'] = sec['address']
                   placed = True
                   for s in secs_2[::-1]:
                     reg['domains'].insert(i, s[1])
@@ -142,3 +157,13 @@ for el in config['elements']:
             if placed and not dec:
               break
             addr1 = addr2
+          if not placed:
+            secs_2[0][1]['output_sections'][0]['physical_address'] = sec['address']
+            placed = True
+            for s in secs_2:
+              if 'domains' not in reg.keys():
+                reg['domains'] = list()
+              s[1]['output_sections'][0]['physical_address'] = sec['address']
+              reg['domains'].append(s[1])
+
+dump_json(kmem, args.out_kmem)
